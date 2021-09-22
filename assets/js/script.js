1b486b205f0325c8b2c9495f659cb87c60ef8be4
Jfisher37 <joseph.eric.fisher@gmail.com>
2021-09-21 20:30:12 -0400

    finished content generation for today card. Moved part of the function into a separate function to be reused by forecast cards.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,27 @@ let searchButtonEl = document.getElementById('search-button');
 let previousSearchEl = document.getElementById('prev-search-btns');
 let todayCardEl = document.getElementById('weather-today');
 let previousSearches = [];
+let todayUl = document.createElement('ul');
 
-
+function generateAllCards(data, container, name){
+   
+       let weatherIcon = document.createElement('img');
+       console.log(data.weather[0].icon);
+       weatherIcon.setAttribute('src', 'http://openweathermap.org/img/wn/'+ data.weather[0].icon + '@2x.png')
+       name.appendChild(weatherIcon);
+       
+       
+       let tempLi = document.createElement ('li');
+       tempLi.innerHTML = 'Temp: ' + data.main.temp + '°F';
+       todayUl.appendChild(tempLi);
+       let windLi = document.createElement ('li');
+       windLi.innerHTML = 'Wind: ' + data.wind.speed + ' MPH';
+       todayUl.appendChild(windLi);
+       let humidLi = document.createElement ('li');
+       humidLi.innerHTML = 'Humidity: ' + data.main.humidity + '%';
+       todayUl.appendChild(humidLi);
+       container.appendChild(todayUl);
+}
 
 function renderFutureCards(city){
 
@@ -29,35 +48,70 @@ function renderTodayCard (city){
 
     let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=95ef123b38c031799d08dde42cb52cf2&units=imperial';
 
+    let cityName = document.createElement('h1');
+    cityName.innerHTML = city + ' (' + moment().format("MM-DD-YY") + ')';
+    todayCardEl.appendChild(cityName);
+
   fetch(requestUrl)
     .then(function (response) {
       console.log(response);
       return response.json();
     })
-    .then(function (data) {
-        console.log(data);
-       let cityName = document.createElement('h1');
-       cityName.innerHTML = city + ' (' + moment().format("MM-DD-YY") + ')';
-       let weatherIcon = document.createElement('img');
-       console.log(data.weather[0].icon);
-       weatherIcon.setAttribute('src', 'http://openweathermap.org/img/wn/'+ data.weather[0].icon + '@2x.png')
-       cityName.appendChild(weatherIcon);
-       todayCardEl.appendChild(cityName);
-       let todayUl = document.createElement('ul');
-       let tempLi = document.createElement ('li');
-       tempLi.innerHTML = 'Temp: ' + data.main.temp + '°F';
-       todayUl.appendChild(tempLi);
-       let windLi = document.createElement ('li');
-       windLi.innerHTML = 'Wind: ' + data.wind.speed + ' MPH';
-       todayUl.appendChild(windLi);
-       let humidLi = document.createElement ('li');
-       humidLi.innerHTML = 'Humidity: ' + data.main.humidity + '%';
-       todayUl.appendChild(humidLi);
-       todayCardEl.appendChild(todayUl);
+    .then(function (data){
+        generateAllCards(data, todayCardEl, cityName);
+        let latLonUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&appid=95ef123b38c031799d08dde42cb52cf2&units=imperial';
+       
+        fetch(latLonUrl)
+            .then(function (responseLL) {
+                console.log(responseLL);
+                return responseLL.json();
+                })
+            .then(function (dataLL) {
+                let uvLi = document.createElement ('li');
+                console.log(dataLL);
+                uvLi.innerHTML = 'UV Index: ' + dataLL.current.uvi;
+                console.log(dataLL.current.uvi);
+                if (dataLL.current.uvi < 3) {
+                    uvLi.setAttribute('style', 'background-color: green; display: inline-block; padding: 1px; border-radius: 4px')
+                }
+                else if (dataLL.current.uvi >= 3 && dataLL.current.uvi < 8) {
+                    uvLi.setAttribute('style', 'background-color: orange; display: inline-block; padding: 1px; border-radius: 4px')
+                }
+                else if (dataLL.current.uvi >= 8) {
+                    uvLi.setAttribute('style', 'background-color: orange; display: inline-block; padding: 1px; border-radius: 4px')
+                }
+                todayUl.appendChild(uvLi);
+    }); 
+
+    //     console.log(data);
+    //    let cityName = document.createElement('h1');
+    //    cityName.innerHTML = city + ' (' + moment().format("MM-DD-YY") + ')';
+    //    let weatherIcon = document.createElement('img');
+    //    console.log(data.weather[0].icon);
+    //    weatherIcon.setAttribute('src', 'http://openweathermap.org/img/wn/'+ data.weather[0].icon + '@2x.png')
+    //    cityName.appendChild(weatherIcon);
+    //    todayCardEl.appendChild(cityName);
+    //    let todayUl = document.createElement('ul');
+    //    let tempLi = document.createElement ('li');
+    //    tempLi.innerHTML = 'Temp: ' + data.main.temp + '°F';
+    //    todayUl.appendChild(tempLi);
+    //    let windLi = document.createElement ('li');
+    //    windLi.innerHTML = 'Wind: ' + data.wind.speed + ' MPH';
+    //    todayUl.appendChild(windLi);
+    //    let humidLi = document.createElement ('li');
+    //    humidLi.innerHTML = 'Humidity: ' + data.main.humidity + '%';
+    //    todayUl.appendChild(humidLi);
+    //    todayCardEl.appendChild(todayUl);
+
+       
+       
+     
+       
        
       })
+    };
 
-};
+// };
 
     // create button functionality
 searchButtonEl.onclick = function(){
